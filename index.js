7d51900e0b3a7a72ed85573c9aa82102cc1f68cd
Thomas Card <tcardlab@gmail.com>
2023-10-25 19:47:15 -0400

    Feat: generate docs from cli scraping

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ function getCmdNames(cmdHelpStr) {
 function formatCmd(name, cmdStr, depth) {
   let usageStart = cmdStr.indexOf('\nUsage:')
   let out = ''
-    out += `\n<h${depth+1}>${name}:</h${depth+1}>\n`
-    out += `\n${cmdStr.substring(0, usageStart)}\n`
+    out += `\n<h${depth+2}>${name}</h${depth+2}>\n`
+    out += `\n${cmdStr
+      .substring(0, usageStart)
+      .replaceAll(/\n\t./g, '\n- ')
+      .replaceAll(/\n[^-]/g, '\n\n<br/>')
+    }\n`
     out += `\n\`\`\`${cmdStr.substring(usageStart)}\`\`\`\n`
   return out
 }
@@ -42,49 +46,70 @@ async function recursiveDocs(commandNames, depth=0, parents=[]) {
     if (cmd==='help') continue;
 
     let formattedCMD = `spacetime help ${cmd==='help' ? '' : [...parents, cmd].join(' ')}`
-    console.log(formattedCMD, parents, cmd)
 
-    let cmdRes = execPro(formattedCMD)
-    docObj[cmd] = cmdRes
-    cmdRes.then(async (v) => {
-      let subCmdNames = getCmdNames(v.stdout) || []
-      //console.log('Sub:', subCmdNames)
-      //console.log(depth, cmd, subCmdNames)
-      //console.log( formatCmd(cmd, v.stdout, depth) )
+    let cmdRes = await execPro(formattedCMD)
+    let subCmdNames = getCmdNames(cmdRes.stdout) || []
 
-      docObj[cmd]={
-        content: v.stdout,
-        subCmdNames,
-        formatted: formatCmd(cmd, v.stdout, depth),
-        subCmds: subCmdNames.length ? await recursiveDocs(subCmdNames, depth+1, [...parents, cmd]) : [],
-        depth
-      }
-    })
+    docObj[cmd] = {
+      content: cmdRes.stdout,
+      subCmdNames,
+      //formattedCMD,
+      formatted: formatCmd(cmd, cmdRes.stdout, depth),
+      subCmds: subCmdNames.length ? await recursiveDocs(subCmdNames, depth+1, [...parents, cmd]) : {},
+      depth
+    }
+
   }
-  // top level cmds resolve b4 children, so we have an issue
-  await Promise.all(Object.values(docObj))
+
   return docObj
 }
 
 
 function createFile(content) {
-  fs.mkdir('dist')
-  fs.writeFile('dist/docs', content, 'utf8', (err) => {
+  if (process.argv.includes('build')) {
+    fs.writeFile('docs.md', content, 'utf8', (err) => {
+      if (err) console.error('Error writing the edited content to the file:', err)
+      console.log('File edited successfully.');
+    })
+    return
+  } 
+
+  fs.mkdir('dev', { recursive: true }, (err) => {})
+  fs.writeFile('dev/docs.md', content, 'utf8', (err) => {
     if (err) console.error('Error writing the edited content to the file:', err)
     console.log('File edited successfully.');
   })
+}
+
+function generateDetails(command) {
+  let output = ''
+  output += command.formatted
+
+  if (Object.keys(command.subCmds).length) {
+    output += `\n<details><summary>Sub Commands</summary>\n`
+    for (const subCommand of Object.values(command.subCmds)) {
+      output += generateDetails(subCommand);
+    }
+    output += `</details>`;
+  }
+
+  return output;
 }
 
 !(async () => {
   // Get top level cmds
   let res = execSync('spacetime help').toString()
   let cmdNameArr = getCmdNames(res)
-  console.log(cmdNameArr)
+  //console.log(cmdNameArr)
 
   let cmdObj = await recursiveDocs(cmdNameArr)
-  console.log(cmdObj)
 
+  
   // format cmdObj to content
-  // ...
-  //createFile(content) 
+  let content = ''
+  for (let [k, v] of Object.entries(cmdObj)) {
+    content += generateDetails(v)
+  }
+  
+  createFile(content) 
 })()
